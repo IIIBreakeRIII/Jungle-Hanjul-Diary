85afc9068e94a444ce62da16214b50831150441a
IIIBreakeRIII <ryu990305@icloud.com>
2025-07-08 22:45:12 +0900

    Main menu, write diary 프론트 코드

--- auth_routes.py
+++ auth_routes.py
@@ -21,4 +21,19 @@ def register_auth_routes(app):
   def login():
     # 예시 로그인 로직
     return jsonify({"message": "로그인 성공"})
+  
 
+  """
+  아래의 코드는 FE 마이그레이션용 임시 코드입니다!
+  BE 개발을 진행할 시에 삭제하여 사용하면 됩니다.
+
+  함수 이름의 시작이 temp_ 인 경우, FE에서 만든 임시 코드입니다.
+  """
+
+  @app.route('/menu-main', methods=['GET'])
+  def temp_menu_main_render():
+    return render_template('menu-main.html')
+
+  @app.route('/menu-writeDiary', methods=['GET'])
+  def temp_menu_writeDiary():
+    return render_template('menu-writeDiary.html')
